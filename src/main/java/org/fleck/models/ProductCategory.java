4e12872a3eefc514f8fa9c3b53729affe790f269
Marc-Julian Fleck <marcjulian.fleck@web.de>
2022-02-21 18:15:17 +0100

    Implemented getProductCategory() in ProductCategory Enum

--- src/main/java/org/fleck/models/ProductCategory.java
+++ src/main/java/org/fleck/models/ProductCategory.java
@@ -1,5 +1,7 @@
 package org.fleck.models;
 
+import java.util.regex.Pattern;
+
 public enum ProductCategory {
     BOOK(false),
     FOOD(false),
@@ -8,13 +10,26 @@ public enum ProductCategory {
     BODYCARE(true),
     UNKNOWN(true);
 
-    boolean taxable;
+    public final boolean taxable;
 
     ProductCategory(boolean taxable) {
         this.taxable = taxable;
     }
 
     public static ProductCategory getProductCategory(String productName) {
-        return BOOK;
+        if (Pattern.compile("pills|headache").matcher(productName).find()) {
+            return ProductCategory.MEDICAL;
+        } else if (Pattern.compile("chocolate").matcher(productName).find()) {
+            return ProductCategory.FOOD;
+        } else if(Pattern.compile("perfume").matcher(productName).find()) {
+            return ProductCategory.BODYCARE;
+        } else if(Pattern.compile("book").matcher(productName).find()) {
+            return ProductCategory.BOOK;
+        } else if(Pattern.compile("CD|music").matcher(productName).find()) {
+            return ProductCategory.ENTERTAINMENT;
+        } else {
+            return ProductCategory.UNKNOWN;
+        }
+
     }
 }
