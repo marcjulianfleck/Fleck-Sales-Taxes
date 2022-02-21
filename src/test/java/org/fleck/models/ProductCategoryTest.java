0bee189665eaf3b03d5d47848af82171813be072
Marc-Julian Fleck <marcjulian.fleck@web.de>
2022-02-21 16:48:58 +0100

    Implemented testGetProductCategory() & testIsProductCategoryTaxable() TestCases

--- src/test/java/org/fleck/models/ProductCategoryTest.java
+++ src/test/java/org/fleck/models/ProductCategoryTest.java
@@ -5,7 +5,23 @@ import junit.framework.TestCase;
 public class ProductCategoryTest extends TestCase {
 
     public void testGetProductCategory() {
-        assertEquals(true,true);
-        // TODO: Implement Tests
+        assertEquals(ProductCategory.BOOK, ProductCategory.getProductCategory("book"));
+        assertEquals(ProductCategory.ENTERTAINMENT, ProductCategory.getProductCategory("music CD"));
+        assertEquals(ProductCategory.FOOD, ProductCategory.getProductCategory("chocolate bar"));
+        assertEquals(ProductCategory.FOOD, ProductCategory.getProductCategory("box of chocolates"));
+        assertEquals(ProductCategory.BODYCARE, ProductCategory.getProductCategory("bottle of perfume"));
+        assertEquals(ProductCategory.MEDICAL, ProductCategory.getProductCategory("headache pills"));
+        assertEquals(ProductCategory.UNKNOWN, ProductCategory.getProductCategory("beer"));
     }
+
+    public void testIsProductCategoryTaxable() {
+        assertTrue(ProductCategory.ENTERTAINMENT.taxable);
+        assertTrue(ProductCategory.BODYCARE.taxable);
+        assertTrue(ProductCategory.UNKNOWN.taxable);
+
+        assertFalse(ProductCategory.BOOK.taxable);
+        assertFalse(ProductCategory.FOOD.taxable);
+        assertFalse(ProductCategory.MEDICAL.taxable);
+    }
+
 }
