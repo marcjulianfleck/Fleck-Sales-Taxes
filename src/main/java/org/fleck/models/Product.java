e78ff2ab5c42d36eea84c8ad17c7e331a0571f6f
Marc-Julian Fleck <marcjulian.fleck@web.de>
2022-02-22 09:10:07 +0100

    Implemented getGrossPrice() & getAllAppliedTaxes() of the product class together with a few helper methods and comments.

--- src/main/java/org/fleck/models/Product.java
+++ src/main/java/org/fleck/models/Product.java
@@ -1,11 +1,14 @@
 package org.fleck.models;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Objects;
 
 public final class Product {
 
-    private final String IMPORTED_PREFIX = "imported ";
+    public static final String IMPORTED_PREFIX = "imported ";
+    public static final BigDecimal BASIC_SALES_TAX_RATE = new BigDecimal(10);
+    public static final BigDecimal IMPORTED_SALES_TAX_RATE = new BigDecimal(5);
 
     private final String productName;
     private final ProductCategory productCategory;
@@ -16,21 +19,73 @@ public final class Product {
         this.productName = productName;
         this.productCategory = ProductCategory.getProductCategory(this.productName);
         this.imported = imported;
-        this.netPrice = new BigDecimal(netPrice);
+        this.netPrice = new BigDecimal(netPrice).setScale(2, RoundingMode.HALF_UP);
     }
 
+    /**
+     *This method returns the product name of the product with the prefix "imported" if applicable.
+     *
+     * @return the product name with the prefix "imported" if applicable as a String.
+     */
     public String getProductName() {
         return imported ? IMPORTED_PREFIX + productName : productName;
     }
 
+    /**
+     *This method returns the gross price of the product correctly rounded.
+     *
+     * @return the gross price of the product correctly rounded as a Double.
+     */
     public Double getGrossPrice() {
-        // TODO: calc and Round properly
-        return 12.49;
+        return  netPrice.add(calcBasicSalesTaxes().add(calcImportedSalesTaxes())).doubleValue();
     }
 
+    /**
+     *This method returns just the applied taxes of the product correctly rounded.
+     *
+     * @return the applied taxes of the product correctly rounded as a Double.
+     */
     public Double getAllAppliedTaxes() {
-        // TODO: calc and round properly
-        return 0.00;
+        return  calcBasicSalesTaxes().add(calcImportedSalesTaxes()).doubleValue();
+    }
+
+    /**
+     *This method returns the basic sales taxes of the product (if applicable) correctly rounded up to the nearest 0.05.
+     *
+     * @return the basic sales taxes of the product correctly rounded up to the nearest 0.05 as a {@code "BigDecimal"}.
+     */
+    private BigDecimal calcBasicSalesTaxes() {
+        BigDecimal basicSalesTaxes = productCategory.taxable
+                ? netPrice.multiply(BASIC_SALES_TAX_RATE).divide(new BigDecimal(100))
+                : BigDecimal.ZERO;
+
+        return roundToNearest5ct(basicSalesTaxes.setScale(2, RoundingMode.HALF_UP));
+    }
+
+    /**
+     *This method returns the imported sales taxes of the product (if applicable) correctly rounded up to the nearest 0.05.
+     *
+     * @return the  imported sales taxes of the product correctly rounded up to the nearest 0.05 as a {@code "BigDecimal"}.
+     */
+    private BigDecimal calcImportedSalesTaxes() {
+        BigDecimal importedSalesTaxes = imported
+                ? netPrice.multiply(IMPORTED_SALES_TAX_RATE).divide(new BigDecimal(100))
+                : BigDecimal.ZERO;
+
+        return roundToNearest5ct(importedSalesTaxes.setScale(2, RoundingMode.HALF_UP));
+    }
+
+    /**
+     *This method rounds the inputted value ({@code "BigDecimal"}) up to the nearest 0.05 and returns it.
+     *
+     * @param  value
+     *         A ({@code "BigDecimal"}) which should be rounded up to the nearest 0.05.
+     *
+     * @return the inputted value correctly rounded up to the nearest 0.05 as a {@code "BigDecimal"}.
+     */
+    private BigDecimal roundToNearest5ct(BigDecimal value) {
+        Double nearest5ct = Math.ceil((value.doubleValue() * 20.0)) / 20.0;
+        return new BigDecimal(nearest5ct).setScale(2, RoundingMode.HALF_UP);
     }
 
     @Override
