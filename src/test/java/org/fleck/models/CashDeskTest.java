d7a220873eab98f40e3aadda038f95e62fff9ebb
Marc-Julian Fleck <marcjulian.fleck@web.de>
2022-02-23 09:57:07 +0100

    Fixed CashDeskTest

--- src/test/java/org/fleck/models/CashDeskTest.java
+++ src/test/java/org/fleck/models/CashDeskTest.java
@@ -13,8 +13,7 @@ public class CashDeskTest extends TestCase{
             "1 book: 9.99\n" +
             "1 imported bottle of perfume: 83.00\n" +
             "Sales Taxes: 10.90\n" +
-            "Total: 92,99\n" +
-            "-----------------------------------\n";
+            "Total: 92.99\n";
 
     private final Product testProductBook = new Product("book", false, 9.99);
     private final Product testProductPerfumeImported = new Product("bottle of perfume", true, 72.10);
@@ -24,22 +23,36 @@ public class CashDeskTest extends TestCase{
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
 
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        importedTestBasket = CashDesk.getInstance().scanNewBasket("assets/testInput.txt");
+        System.setOut(new PrintStream(outContent));
+    }
+
     public void testStaticGetInstance() {
         assertEquals(CashDesk.getInstance(), CashDesk.getInstance());
     }
 
     public void testScanNewBasket() throws IOException {
-        importedTestBasket = CashDesk.getInstance().scanNewBasket("assets/testInput.txt");
         assertEquals(2, importedTestBasket.size());
         assertTrue(importedTestBasket.contains(testProductBook));
         assertTrue(importedTestBasket.contains(testProductPerfumeImported));
     }
 
     public void testCheckoutAndPrintReceiptOfPurchase() {
-        System.setOut(new PrintStream(outContent));
         CashDesk.getInstance().checkoutAndPrintReceiptOfPurchase();
+
         assertEquals(EXPECTED_CONSOLE_OUTPUT, outContent.toString());
         assertEquals(0, importedTestBasket.size());
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+
+        importedTestBasket.clear();
         System.setOut(originalOut);
     }
 }
